6c230b1b9030bee5b5aa034c8058b268e61da125
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-12-11 21:55:05 +0530

    Implement dynamic base URL for client configuration

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,14 @@
 import { treaty } from '@elysiajs/eden'
 import type { app } from '../app/api/[[...slugs]]/route'
 
-// this require .api to enter /api prefix
-export const client = treaty<app>('localhost:3000').api
+const getBaseUrl = () => {
+  if (typeof window !== 'undefined') {
+    return window.location.origin
+  }
+  return process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_URL 
+    ? `https://${process.env.VERCEL_URL}` 
+    : 'http://localhost:3000'
+}
+
+export const client = treaty<app>(getBaseUrl()).api
 
